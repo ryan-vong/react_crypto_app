d00f779d56724e0affd677e0910479ae95a38d91
Ryan Vong <ryan.vong88@gmail.com>
2018-11-14 15:42:19 -0800

    ReactJS app commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ReactJS app - list of crypto-currencies
---
+++ src/components/detailcurrency/DetailCurrency.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { RootURL } from '../../config';
+import Load from '../common/Load';
+import { handleResponse, calcChangePercent } from '../../helper';
+import './DetailCurrency.css';
+
+class DetailCurrency extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currency: {},
+            loading: false,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        const currencyID = this.props.match.params.id;
+
+        this.setState({ loading: true });
+
+        fetch(`${RootURL}/cryptocurrencies/${currencyID}`)
+           .then(handleResponse)
+           .then((currency) => {
+               this.setState({
+                   loading: false,
+                   error: null,
+                   currency: currency,
+               });
+           })
+           .catch((error) => {
+               this.setState({
+                   loading: false,
+                   error: error.errorMessage,
+               });
+           });
+    }
+
+    render() {
+        const { loading, error, currency } = this.state;
+
+        if (loading) {
+            return <div className="loading-container"><Load /></div>
+        }
+
+        if (error) {
+            return <div className="error">{error}</div>
+        }
+
+        return (
+            <div className="dt">
+               <h1 className="dt-heading">{currency.name} ({currency.symbol})</h1>
+
+               <div className="dt-container">
+                  <div className="dt-item">
+                     Price <span className="dt-value">$ {currency.price}</span>
+                  </div>
+                  <div className="dt-item">
+                     Rank <span className="dt-value">{currency.rank}</span>
+                  </div>
+                  <div className="dt-item">
+                     24-Hour Change <span className="dt-value">{calcChangePercent(currency.percentChange24h)}</span>
+                  </div>
+                  <div className="dt-item">
+                     <span className="dt-title">Market cap</span><span className="dt-dollar">$</span>{currency.marketCap}
+                  </div>
+                  <div className="dt-item">
+                     <span className="dt-title">24 Hour Vol</span><span className="dt-dollar">$</span>{currency.volume24h}
+                  </div>
+                  <div className="dt-item">
+                     <span className="dt-title">Tot Supply</span>{currency.totalSupply}
+                  </div>
+               </div>
+            </div>
+        );
+    }
+}
+
+export default DetailCurrency;
